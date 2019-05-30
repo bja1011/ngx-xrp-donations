3d905b5224ccdcc880c5007785e6a1243fdf7739
adam <kaczmarek@oskarwegner.pl>
2019-05-30 15:31:04 +0200

    Websocket connection with ripple server.

--- projects/xrp-donations/src/lib/xrp-donations.component.ts
+++ projects/xrp-donations/src/lib/xrp-donations.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ConnectionStatus, XrpDonationsService } from './xrp-donations.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-xrp-donations',
   template: `
-    <p>
-      xrp-donations works!
-    </p>
+    {{balance ? balance + 'XRP' : '-'}} <br>{{status}}
   `,
   styles: []
 })
 export class XrpDonationsComponent implements OnInit {
 
-  constructor() {
+  @Input() account: string;
+  balance: number;
+  status: ConnectionStatus = ConnectionStatus.disconnected;
+
+  constructor(private xrpDonationsService: XrpDonationsService,
+  ) {
+    xrpDonationsService.connect()
+      .subscribe(msg => {
+        console.log(msg);
+        this.handleSocketResponse(msg);
+      });
+
+    xrpDonationsService.openObserver
+      .pipe(
+        take(1)
+      )
+      .subscribe((event) => {
+        this.status = ConnectionStatus.connected;
+        // xrpDonationsService.watchAccount(this.account);
+        xrpDonationsService.getAccount(this.account);
+      });
   }
 
   ngOnInit() {
   }
 
+  private handleSocketResponse(msg: any) {
+    const accountData = msg.result.account_data;
+    if (accountData) {
+      this.balance = accountData.Balance / 1000000;
+    }
+  }
 }
+
--- projects/xrp-donations/src/lib/xrp-donations.service.ts
+++ projects/xrp-donations/src/lib/xrp-donations.service.ts
@@ -1,10 +1,56 @@
 import { Injectable } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
+import { WebSocketSubject } from 'rxjs/src/internal/observable/dom/WebSocketSubject';
+
+const rippleServer = 'wss://s2.ripple.com:443';
 
 @Injectable({
   providedIn: 'root'
 })
 export class XrpDonationsService {
 
+  socket: Subject<any>;
+  openObserver: Subject<Event> = new Subject<Event>();
+
   constructor() {
   }
+
+  connect() {
+    return this.socket = webSocket({
+      url: rippleServer,
+      openObserver: this.openObserver
+    });
+  }
+
+  getAccount(account: string) {
+    this.socket.next({
+      command: 'account_info',
+      account,
+      strict: true,
+      ledger_index: 'validated'
+    });
+  }
+
+  watchAccount(account: string) {
+    this.socket.next({
+      command: 'subscribe',
+      accounts: [account],
+      streams: [
+        'ledger'
+      ]
+    });
+  }
+
+  ping() {
+    const command = {
+      command: 'ping'
+    };
+    this.socket.next(command);
+  }
+}
+
+export enum ConnectionStatus {
+  connected = 'Connected',
+  disconnected = 'Disconnected'
 }
